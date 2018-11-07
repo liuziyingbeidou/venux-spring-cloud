ae8fffbb04af1b285eadb642dfbe230b7a11d805
liuzy <liuziyingbeidou@163.com>
2018-11-07 16:04:36 +0800

    turbine

--- venux-spring-cloud-hystrix-dashboard/src/main/java/org/venux/hystrix/dashboard/VenuxHystrixDashboardApplication.java
+++ venux-spring-cloud-hystrix-dashboard/src/main/java/org/venux/hystrix/dashboard/VenuxHystrixDashboardApplication.java
@@ -3,6 +3,7 @@ package org.venux.hystrix.dashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
  * @Title: ZJS 
@@ -14,6 +15,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
  */
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableTurbine
 public class VenuxHystrixDashboardApplication {
 	
 	public static void main(String[] args) {
